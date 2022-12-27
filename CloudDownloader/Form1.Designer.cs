33a61b8cd7c5377c9756daa33b15bf1e85510611
Kawshik Roy <kawshik.zero@yahoo.com>
2022-12-28 04:47:10 +1100

    application added

---
+++ CloudDownloader/Form1.Designer.cs
@@ -0,0 +1,346 @@
+﻿namespace CloudDownloader
+{
+    partial class DownloadForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DownloadForm));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.BtnSaveLocation = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(177, 23);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(717, 453);
+            this.panel1.TabIndex = 0;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(177, 23);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(712, 453);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.BtnSaveLocation);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage1.Size = new System.Drawing.Size(704, 427);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "URL";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // BtnSaveLocation
+            // 
+            this.BtnSaveLocation.Location = new System.Drawing.Point(604, 205);
+            this.BtnSaveLocation.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.BtnSaveLocation.Name = "BtnSaveLocation";
+            this.BtnSaveLocation.Size = new System.Drawing.Size(91, 37);
+            this.BtnSaveLocation.TabIndex = 6;
+            this.BtnSaveLocation.Text = "Save Path";
+            this.BtnSaveLocation.UseVisualStyleBackColor = true;
+            this.BtnSaveLocation.Click += new System.EventHandler(this.BtnSaveLocation_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(169, 161);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(604, 164);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 37);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Cancel Download";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(604, 123);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 37);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Download";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(4, 124);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "URL";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(68, 124);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(532, 32);
+            this.textBox1.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.listView1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage2.Size = new System.Drawing.Size(704, 427);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Downloads";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(705, 448);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged_1);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "File Name";
+            this.columnHeader1.Width = 119;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "File Type";
+            this.columnHeader2.Width = 94;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "File Size";
+            this.columnHeader3.Width = 111;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Status";
+            this.columnHeader4.Width = 107;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Location";
+            this.columnHeader5.Width = 226;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(-4, 23);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(181, 182);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Location = new System.Drawing.Point(0, 24);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(887, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.optionsToolStripMenuItem,
+            this.helpToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(887, 24);
+            this.menuStrip2.TabIndex = 3;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.optionsToolStripMenuItem.Text = "Options";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            // 
+            // DownloadForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(887, 476);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.menuStrip2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "DownloadForm";
+            this.Text = "Cloud Downloader";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button BtnSaveLocation;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+    }
+}
+
---
+++ CloudDownloader/Form1.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Threading;
+using System.Collections;
+using Microsoft.Win32;
+using System.Diagnostics;
+
+namespace CloudDownloader
+{
+    public partial class DownloadForm : Form
+    {
+        //Declaring global variables
+        private string Url;
+        private string fextension;
+        private string fName;
+        private long fSize;
+        private string sPrefix;
+        private string Slocation = getDownloadFolderPath() + "/";
+        private string size;
+        private bool existenceCheck;
+        private string status;
+        private int currDownloads;
+        private int maxDownloads = 4;
+        Stopwatch sw = new Stopwatch();
+        Thread thread;
+
+        public string url { get => Url; set => Url = value; }
+        public long FSize { get => fSize; set => fSize = value; }
+        public string FName { get => fName; set => fName = value; }
+
+        public DownloadForm()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {    
+            thread = new Thread(new ThreadStart(DownloadsInfo));
+            if (currDownloads < maxDownloads)
+            {
+                ++currDownloads;
+
+                //Inititating timer and starting the thread
+                timer1.Start();
+                thread.Start();
+            }
+            else
+            {
+                MessageBox.Show("You have reached the maximum number of downloads, Please wait to finish or about a download.");
+                return;
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (existenceCheck)
+            {
+
+                string message = "Would you like to adort the current download?";
+
+                var result = MessageBox.Show(message, "File name conflict: " + (@Slocation + fName + fextension), MessageBoxButtons.OKCancel);
+
+                if (result == System.Windows.Forms.DialogResult.OK)
+                {
+                    thread.Abort();
+                    existenceCheck = false;
+                    MessageBox.Show("Download Cancelled");
+                }
+                else
+                {
+                    //Exiting the method
+                    return;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Currenly, you have no active downloads");
+            }
+
+        }
+
+        private void DownloadsInfo() {
+            Url = textBox1.Text;
+
+           
+            if (Url == null || Url == "")
+            {
+                MessageBox.Show("INVALID URL, PLEASE ENTER VALID URL");
+            }
+
+            else
+            {
+                
+                int index = Url.LastIndexOf(".");
+
+                
+                fextension = ".";
+
+                
+                for (int x = 1; x < 4; x++)
+                {
+                    fextension = fextension + string.Concat(Url[index + x]);
+                }
+
+                
+                if (fextension == ".exe" || fextension == ".jpg" || fextension == ".png" || fextension == ".mp3" || fextension == ".mp4" || fextension == ".ico")
+                {
+                    
+                    index = Url.LastIndexOf("/");
+
+                    
+                    FName = "";
+
+                   
+                    for (int x = 1; x < 6; x++)
+                    {
+                       
+                        if (Url[index + x] != '.')
+                            FName = FName + string.Concat(Url[index + x]);
+                        
+                        else
+                        {
+                            FName = "Downloaded_";
+                        }
+                    }
+
+                    try
+                    {
+                        
+                        if (File.Exists(@Slocation + FName + fextension))
+                        {
+
+                            string message = "A file with the same name already exists, " + "Would you like to redownload it? ";
+
+                            var result = MessageBox.Show(message, "File name conflict: " + (@Slocation + FName + fextension), MessageBoxButtons.OKCancel);
+
+                            if (result == System.Windows.Forms.DialogResult.OK)
+                            {
+                                File.Delete((@Slocation + FName + fextension));
+                            }
+                            else
+                            {
+                                //exits the execution
+                                return;
+                            }
+                        }
+                    }
+                    
+                    catch (Exception)
+                    {
+                        MessageBox.Show("File is already being downloaded");
+                    }
+
+                    
+                    existenceCheck = true;
+
+                    try
+                    {
+                        using (WebClient Client = new WebClient())
+                        {
+
+                            status = "Downloading";
+
+                            Client.DownloadFile(url, @Slocation + FName + fextension);
+
+                           
+                            Int64 fileSize = Convert.ToInt64(Client.ResponseHeaders["Content-Length"]);
+
+                           
+                            if (fileSize > 1099510579199)
+                            {
+                                fileSize = ((((fileSize / 1024) / 1024) / 1024) / 1024);
+                                sPrefix = "TB";
+                            }
+                            if (fileSize > 1073740800)
+                            {
+                                fileSize = ((fileSize / 1024) / 1024) / 1024;
+                                sPrefix = "GB";
+                            }
+                            if (fileSize > 1048575)
+                            {
+                                fileSize = (fileSize / 1024) / 1024;
+                                sPrefix = "MB";
+                            }
+                            else if (fileSize > 1023)
+                            {
+                                fileSize = fileSize / 1024;
+                                sPrefix = "KB";
+                            }
+                            else
+                            {
+                                sPrefix = "Bytes";
+                            }
+
+                            size = fileSize.ToString() + sPrefix;
+
+                            status = "Completed";
+                            MessageBox.Show("COMPLETED Download File: " + FName + fextension);
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        
+                     
+                    }
+
+                    
+                    existenceCheck = false;
+
+                    --currDownloads;
+
+                    thread.Join();
+                }
+                else
+                {
+                    
+                    MessageBox.Show("ERROR 404:  URL NOT SUPPORTED!");
+                }
+            }
+        }
+
+        //Used to handle the list view
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var selectedItem = listView1.SelectedItems; //A variable is created in order to store the list view items
+
+            System.Diagnostics.Process.Start(@Slocation + FName + fextension);
+        }
+
+        private void BtnSaveLocation_Click(object sender, EventArgs e)
+        {
+            var dialog = new FolderBrowserDialog();
+            var result = dialog.ShowDialog();
+            if (result == DialogResult.OK)
+                Slocation = dialog.SelectedPath + "/";
+        }
+
+        //The users current directory is returned in this method
+        static string getDownloadFolderPath()
+        {
+            return Registry.GetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders", "{374DE290-123F-4565-9164-39C4925E467B}", String.Empty).ToString();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //Timer to refresh the list view every 100ms
+            if (thread != null)
+            {
+                
+                if (existenceCheck == true) //Checking if the boolean which shows the existence of the file is true or not before updating
+                {
+                    //Appending data to the list view
+                    String[] row = { FName, fextension, size, status, Slocation };//Adding data to the list
+                    var listViewItem = new ListViewItem(row);
+
+                    var downloadingListView = new ListViewItem("Downloading");
+
+                    if (status == "Downloading")
+                    {
+                        label2.Text = "Downloading " + currDownloads + " Files";
+
+                    }
+
+                    if (status != "Downloading")
+                    {
+                        listView1.Items.Add(listViewItem);
+                        timer1.Stop();
+                    }
+                }
+            }
+
+        }
+
+        private void listView1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
